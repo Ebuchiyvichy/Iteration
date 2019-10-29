ee948625d91fbc23ad05ff065e513e3083f869af
ehell42 <55402050+ehell42@users.noreply.github.com>
2019-10-29 20:21:22 +0300

    С again

--- Zeldel.h
+++ Zeldel.h
@@ -2,13 +2,13 @@
 
 void printC(const VectorMatrix<mytype> &C)
 {
-  for (int i = 0; i != C.count; i++)
-  {
-    for (int j = 0; j != 3; j++)
-      std::cout << C.value[j][i] << "   ";
-    std::cout << std::endl;
-    }
-  }
+	for (int i = 0; i != C.count; i++)
+	{
+		for (int j = 0; j != 3; j++)
+			std::cout << C.value[j][i] << "   ";
+		std::cout << std::endl;
+	}
+}
 
 // VectorMatrix<mytype> countbigmatrix(const VectorMatrix<mytype> & A, mytype omega)
 // {
@@ -90,95 +90,94 @@ void printC(const VectorMatrix<mytype> &C)
 
 std::vector<mytype> SmallZeydel(const VectorMatrix<mytype> & A)
 {
-  std::vector<mytype> x(A.rvalue);
-  std::vector<mytype> p(A.rvalue);
-  VectorMatrix<mytype> C(A.count);
-  //start iteration process
-  do
-  {
-    for (int i = 0; i != A.count; i++)
-      p[i] = x[i];
-    for (int i = 0; i != A.count; i++)
-    {
-      double sum = 0;
-      for (int j = 0; j < i; j++)
-      {
-        sum +=(A.value[i][j]*x[j]);
-        C.value[i][j] = (-1)*A.value[i][j]/A.value[i][i];
-      }
-      C.value[i][i] = 0;
-      for (int j = i + 1; j != A.count; j++)
-      {
-        sum += (A.value[i][j] * p[j]);
-        C.value[i][j] = (-1)*A.value[i][j]/A.value[i][i];
-      }
-      x[i]= (A.rvalue[i]-sum) / A.value[i][i];
-    }
-  }
-  while (cube_vect_norm(diff_vector(x,p,A.count), A.count) > ((1 - norm_cube(C)) / norm_cube(C)) * EPS);
-  return x;
+	std::vector<mytype> x(A.rvalue);
+	std::vector<mytype> p(A.rvalue);
+	VectorMatrix<mytype> C(A.count);
+	//start iteration process
+	do
+	{
+		for (int i = 0; i != A.count; i++)
+			p[i] = x[i];
+		for (int i = 0; i != A.count; i++)
+		{
+			double sum = 0;
+			for (int j = 0; j < i; j++)
+			{
+				sum += (A.value[i][j] * x[j]);
+				C.value[i][j] = (-1)*A.value[i][j] / A.value[i][i];
+			}
+			C.value[i][i] = 0;
+			for (int j = i + 1; j != A.count; j++)
+			{
+				sum += (A.value[i][j] * p[j]);
+				C.value[i][j] = (-1)*A.value[i][j] / A.value[i][i];
+			}
+			x[i] = (A.rvalue[i] - sum) / A.value[i][i];
+		}
+	} while (cube_vect_norm(diff_vector(x, p, A.count), A.count) > ((1 - norm_cube(C)) / norm_cube(C)) * EPS);
+	return x;
 }
 
 
 VectorMatrix<mytype> countmatrix(const VectorMatrix<mytype>& A, double omega)
 {
-  VectorMatrix<mytype> C(A.count);
-  for (int k = 0; k != A.count; k++)
-  {
-    std::vector<mytype> x(A.count);
-    std::vector<mytype> p(A.count);
-    x[k] = 1;
-    p[k] = 1;
-    // for (int i = 0; i != A.count; i++)
-    //   p[i] = x[i];
-    for (int i = 0; i != A.count; i++)
-    {
-      double sum = 0;
-      for (int j = 0; j < i; j++)
-        sum +=(omega * A.value[i][j] * x[j])/A.value[i][i];
+	VectorMatrix<mytype> C(A.count);
+	for (int k = 0; k != A.count; k++)
+	{
+		std::vector<mytype> x(A.count);
+		std::vector<mytype> p(A.count);
+		x[k] = 1;
+		p[k] = 1;
+		// for (int i = 0; i != A.count; i++)
+		//   p[i] = x[i];
+		for (int i = 0; i != A.count; i++)
+		{
+			double sum = 0;
+			for (int j = 0; j < i; j++)
+				sum += (omega * A.value[i][j] * x[j]) / A.value[i][i];
 
-      for (int j = i + 1; j != A.count; j++)
-        sum += (omega * A.value[i][j] * p[j])/A.value[i][i];
-      x[i]= (1 - omega) * p[i] - sum;
-    }
-    for (int i = 0; i != A.count; i++)
-    {
-      C.value[i][k] = x[i];
-    }
-  }
-  return (C);
+			for (int j = i + 1; j != A.count; j++)
+				sum += (omega * A.value[i][j] * p[j]) / A.value[i][i];
+			x[i] = (1 - omega) * p[i] - sum;
+		}
+		for (int i = 0; i != A.count; i++)
+		{
+			C.value[i][k] = x[i];
+		}
+	}
+	return (C);
 }
 
- std::vector<mytype> SmallRelax(const VectorMatrix<mytype> & A)
- {
-   double omega = 0;
-   std::vector<mytype> x(A.rvalue);
-   std::vector<mytype> p(A.rvalue);
-   //VectorMatrix<mytype> C(countmatrix(A, omega));
-   do
-   {
-     int n = 0;
-     while (norm_cube(countmatrix(A, omega)) >= 1)
-     {
-       std::cout << norm_cube(countmatrix(A, omega)) << "\n";
-       n++;
-       std::cout << omega << std::endl;
-     }
-     std::cout << norm_cube(countmatrix(A, omega)) << "\n";
-     std::cout << "Meow\n";
+std::vector<mytype> SmallRelax(const VectorMatrix<mytype> & A)
+{
+	double omega = 0;
+	std::vector<mytype> x(A.rvalue);
+	std::vector<mytype> p(A.rvalue);
+	//VectorMatrix<mytype> C(countmatrix(A, omega));
+	do
+	{
+		int n = 0;
+		while (norm_cube(countmatrix(A, omega)) >= 1)
+		{
+			std::cout << norm_cube(countmatrix(A, omega)) << "\n";
+			n++;
+			std::cout << omega << std::endl;
+		}
+		std::cout << norm_cube(countmatrix(A, omega)) << "\n";
+		std::cout << "Meow\n";
 
-     for (int i = 0; i != A.count; i++)
-       p[i] = x[i];
-     for (int i = 0; i != A.count; i++)
-     {
-      double sum = 0;
-      for (int j = 0; j < i; j++)
-        sum -=(omega * A.value[i][j] * x[j])/A.value[i][i];
-      for (int j = i + 1; j != A.count; j++)
-        sum -= (omega * A.value[i][j] * p[j])/A.value[i][i];
-      sum += ((1-omega) * p[i]);
-      x[i]= ((omega*A.rvalue[i])/A.value[i][i]+sum);
-     }
-   } while (cube_vect_norm(diff_vector(x,p, A.count), A.count) > ((1 - norm_cube(countmatrix(A, omega)) / norm_cube(countmatrix(A, omega))) * EPS));
-   return x;
- }
+		for (int i = 0; i != A.count; i++)
+			p[i] = x[i];
+		for (int i = 0; i != A.count; i++)
+		{
+			double sum = 0;
+			for (int j = 0; j < i; j++)
+				sum -= (omega * A.value[i][j] * x[j]) / A.value[i][i];
+			for (int j = i + 1; j != A.count; j++)
+				sum -= (omega * A.value[i][j] * p[j]) / A.value[i][i];
+			sum += ((1 - omega) * p[i]);
+			x[i] = ((omega*A.rvalue[i]) / A.value[i][i] + sum);
+		}
+	} while (cube_vect_norm(diff_vector(x, p, A.count), A.count) >((1 - norm_cube(countmatrix(A, omega)) / norm_cube(countmatrix(A, omega))) * EPS));
+	return x;
+}
